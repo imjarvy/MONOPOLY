5eb79b14fbd9612f390bf053b4f3adc0d29a827b
darlin4 <darlindavid45@gmail.com>
2025-09-25 23:21:08 -0500

    Board service

--- frontEnd/src/services/boardService.js
+++ frontEnd/src/services/boardService.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Servicio para obtener el tablero y propiedades
 export async function getBoard() {
   try {
@@ -9,16 +9,4 @@ export async function getBoard() {
     console.error(error);
     return null;
   }
-=======
-// Servicio para obtener el tablero y propiedades
-export async function getBoard() {
-  try {
-    const response = await fetch('http://127.0.0.1:5000/board');
-    if (!response.ok) throw new Error('No se pudo obtener el tablero');
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
->>>>>>> jarvy
 }
